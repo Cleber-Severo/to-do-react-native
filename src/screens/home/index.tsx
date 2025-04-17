03b0cc2069b8570a8841d9490ff44aad58c89bfe
Cléber Severo <clebersevero1998@gmail.com>
2025-04-17 06:30:21 -0300

    feat: initial projetc files

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Home = () => {
+	return (
+		<View>
+			<Text>This is the homescreen</Text>
+		</View>
+	);
+};
+
+export default Home;
